0ba9b0a72fffab70dbe915c3e08856d595fa5ac6
fudhoil <fudhoil.10119333@mahasiswa.unikom.ac.id>
2022-11-06 12:21:14 +0700

    export supabase

--- pages/api/supabase.js
+++ pages/api/supabase.js
@@ -3,4 +3,4 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.SUPABASE_URL
 const supabaseKey = process.env.SUPABASE_API_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey)
